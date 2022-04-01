ca52631154f89165ecf3abfa2e9b4e296e213e35
dobrematei14 <dobre.matei.ichb@gmail.com>
2022-04-01 17:48:37 +0200

    Exported the model for later use in the dashboard

--- decisionTree.py
+++ decisionTree.py
@@ -1,25 +1,28 @@
 import pandas as pd
 from sklearn import tree
 import numpy as np
-import matplotlib as mplt
 import math
 import matplotlib.pyplot as plt
-# import sklearn2pmml
+import joblib
 
 nan = float('nan')
 
 # add path to arrived seals here
-datasetArrivedSeals = pd.read_excel(open(r'/Users/andreeazelko/Documents/SoftwareEngineering/SealsProject/Arrived seals 2014-2021.xlsx', 'rb'), sheet_name='Arrived Seals')
+datasetArrivedSeals = pd.read_excel(
+    open(r'C:\Users\dobre\PycharmProjects\SealsProject\Arrived_seals_2014-2021.xlsx', 'rb'),
+    sheet_name='Arrived Seals')
 npthingArrivedSeals = datasetArrivedSeals.to_numpy()
 # add path to client data here
-absolutePath = "/Users/andreeazelko/Documents/SoftwareEngineering/SealsProject/clientData/"
+absolutePath = r"C:\Users\dobre\PycharmProjects\SealsProject\sealData"
+
 
 # Get the initials of the species
 def getSealSpecies(str):
-    if (str == "Phoca Vitulina"):
+    if str == "Phoca Vitulina":
         return "PV"
-    if (str == "Halichoerus Grypus"):
+    if str == "Halichoerus Grypus":
         return "HG"
+
 
 # Get the label based on the string
 # Right now - Everything but released is death so it's a 0
@@ -65,26 +68,27 @@ for i in range(221, 500):
         sealSpecies = getSealSpecies(npthingArrivedSeals[i][6])
         # get rid of T in tag ID
         sealTagWithoutT = sealTag[1:]
-        if (sealTag[1:3] == "20" or sealTag[1:3] == "21"):
-            filename = "20" + sealTag[1:3] + "/" + sealTagWithoutT + " " + sealSpecies + ".xlsx"
+        if sealTag[1:3] == "20" or sealTag[1:3] == "21":
+            filename = "20" + sealTag[1:3] + "\\" + sealTagWithoutT + " " + sealSpecies + ".xlsx"
         else:
-            filename = "20" + sealTag[1:3] + "/" + sealSpecies + sealTagWithoutT + ".xlsx"
-        path = absolutePath + filename
+            filename = "20" + sealTag[1:3] + "\\" + sealSpecies + sealTagWithoutT + ".xlsx"
+        path = absolutePath + "\\" + filename
         dataset = pd.read_excel(path)
-        npthing = dataset.to_numpy()
+        ExcelSealData = dataset.to_numpy()
         sealData = np.array([[0] * 2] * 1)
         # check this out
-        if not (math.isnan(getData(npthing, "WBC", False)) or math.isnan(getData(npthing, "LYMF", False)) or math.isnan(getData(npthing, "LYMF", True))):
+        if not (math.isnan(getData(ExcelSealData, "WBC", False)) or math.isnan(
+                getData(ExcelSealData, "LYMF", False)) or math.isnan(getData(ExcelSealData, "LYMF", True))):
             # sealData[0][0] = getData(npthing, "WBC", False)
             # sealData[0][1] = getData(npthing, "LYMF", False)
-            wbcData = getData(npthing, "WBC", False)
+            wbcData = getData(ExcelSealData, "WBC", False)
             wbcDataArr = np.append(wbcDataArr, wbcData)
             labels = np.append(labels, getLabel(npthingArrivedSeals[i][2]))
 
-            lymfDataNoPerc = getData(npthing, "LYMF", False)
+            lymfDataNoPerc = getData(ExcelSealData, "LYMF", False)
             lymfDataArr = np.append(lymfDataArr, lymfDataNoPerc)
 
-            lymfDataArrPerc = np.append(lymfDataArrPerc, getData(npthing, "LYMF", True))
+            lymfDataArrPerc = np.append(lymfDataArrPerc, getData(ExcelSealData, "LYMF", True))
         else:
             print(path)
     except:
@@ -92,13 +96,14 @@ for i in range(221, 500):
 
 trainingArr = np.vstack((wbcDataArr, lymfDataArr, lymfDataArrPerc)).T
 print(trainingArr)
-wbcDecisionTree = tree.DecisionTreeClassifier()
-wbcDecisionTree = wbcDecisionTree.fit(trainingArr, labels.reshape(-1, 1))
-predictionArr = np.array([11.5, 1.5, 13.1])
-# print(tree.export_text(wbcDecisionTree, show_weights=True))
-plt.figure(figsize=(50, 50))
+SealDecisionTree = tree.DecisionTreeClassifier()
+SealDecisionTree = SealDecisionTree.fit(trainingArr, labels.reshape(-1, 1))
 
-tree.plot_tree(wbcDecisionTree, filled=True)
+predictionArr = np.array([11.5, 1.5, 13.1])
+plt.figure(figsize=(50, 50))
+tree.plot_tree(SealDecisionTree, filled=True)
 plt.savefig(fname='treeOutput.png')
 plt.show()
-print(wbcDecisionTree.predict(predictionArr.reshape(1, -1)))
+print(SealDecisionTree.predict(predictionArr.reshape(1, -1)))
+
+joblib.dump(SealDecisionTree, 'SealDecisionTree.pkl')
