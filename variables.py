c44a23f4f9ccd806ec734e505253bce62e3651f4
AndreeaCZ <andreea.zelko10@gmail.com>
2022-05-04 18:21:08 +0200

    added comment

--- variables.py
+++ variables.py
@@ -6,4 +6,5 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(ROOT_DIR, 'Database/sealPredictionData.db')
 MODEL_PATH = os.path.join(ROOT_DIR, 'Model/SealDecisionTree.pkl')
 ARRIVED_SEALS_PATH = os.path.join(ROOT_DIR, 'Arrived_seals_2014-2021.xlsx')
-CLIENT_DATA_PATH = os.path.join(ROOT_DIR, 'clientData')
+CLIENT_DATA_PATH = os.path.join(ROOT_DIR, 'clientData') # path to client data folder
+# pls rename ur own folder to "clientData"
