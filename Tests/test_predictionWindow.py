5b7b1396ea5ac242c3d77ef585cb3ac82c3dc269
Julia Dahlberg <j.i.k.dahlberg@student.rug.nl>
2022-06-16 20:44:16 +0200

    TST: Added tests for predictionWindow

--- Tests/test_predictionWindow.py
+++ Tests/test_predictionWindow.py
@@ -17,6 +17,8 @@ from GUI.predictionWindow import PredictionWindow
 app = QApplication(sys.argv)
 
 
+defaultFeatureList = ["WBC", "LYMF", "GRAN", "MID", "RBC", "HGB", "MCH", "MCHC", "MPV", "PLT"]
+
 class TestPredictionWindow(TestCase):
     # SET UP:
     def setUp(self):
@@ -25,7 +27,7 @@ class TestPredictionWindow(TestCase):
 
     # TestInitial: title, size , button clicked working?
     def test_title(self):
-        self.assertEqual(self.test_window.windowTitle(), "Run predictions")
+        self.assertEqual(self.test_window.windowTitle(), ("Run predictions"))
 
     def test_size(self):
         self.assertEqual(self.test_window.size(), QSize(600, 700))
@@ -70,44 +72,114 @@ class TestPredictionWindow(TestCase):
             QTest.mouseClick(home_button, Qt.MouseButton.LeftButton)
             self.assertTrue(clickCheck.called)
 
+    # Update features without wbc should be disabled, check they are not enabled
     def test_update_input_and_labels(self):
-        self.fail()
+        self.test_window.featureList = ["WBC"]
+        self.test_window.update_input_and_labels()
 
-    def test_get_seal_tag_popup(self):
-        self.fail()
+        disable_input_list = [self.test_window.lymf_input, self.test_window.gran_input, self.test_window.mid_input, self.test_window.rbc_input,
+                              self.test_window.hgb_input, self.test_window.mch_input, self.test_window.mchc_input, self.test_window.mpv_input, self.test_window.plt_input]
 
+        self.assertTrue(self.test_window.wbc_input.isEnabled())
+
+        for i in disable_input_list:
+            self.assertFalse(i.isEnabled())
+
+    # Check all feature inputs should be empty.
     def test_reset_input_fields_text(self):
-        self.fail()
+        for input in self.test_window.featureInputDict.values():
+            self.assertEqual("", input.text())
 
-    def test_load_default_model_with_new_msg(self):
-        self.fail()
+    @mock.patch("joblib.load")
+    @mock.patch("GUI.predictionWindow.pop_message_box")
+    def test_load_default_model_with_new_msg(self, mock_load, mock_pop):
+        self.test_window.load_default_model_with_new_msg()
 
-    def test_clear_input_fields(self):
-        self.fail()
+        defaultFeatureList = ["WBC", "LYMF", "GRAN", "MID", "RBC", "HGB", "MCH", "MCHC", "MPV", "PLT"]
+
+        self.assertEqual("defaultModel", self.test_window.modelName)
+        self.assertEqual(defaultFeatureList, self.test_window.featureList)
+        mock_load.assert_called_once()
+        mock_pop.assert_called_once()
 
     def test_update_feature_list(self):
-        self.fail()
+        pass
 
     def test_save_results(self):
-        self.fail()
+        pass
 
     def test_save_prediction(self):
-        self.fail()
+        pass
 
-    def test_load_default_model(self):
-        self.fail()
+    @mock.patch("joblib.load")
+    @mock.patch("GUI.predictionWindow.pop_message_box")
+    def test_load_default_model(self, mock_load, mock_pop):
+        self.test_window.load_default_model()
 
-    def test_load_model(self):
-        self.fail()
+        defaultFeatureList = ["WBC", "LYMF", "GRAN", "MID", "RBC", "HGB", "MCH", "MCHC", "MPV", "PLT"]
+
+        self.assertEqual("defaultModel", self.test_window.modelName)
+        self.assertEqual(defaultFeatureList, self.test_window.featureList)
+        mock_load.assert_called_once()
+        mock_pop.assert_called_once()
+
+    # @mock.patch("joblib.load")
+    # @mock.patch("GUI.predictionWindow.pop_message_box")
+    # def test_load_model_success(self, mock_load, mock_update):
+    #     with patch('PyQt6.QtWidgets.QFileDialog.getOpenFileName') as mock_qDialog:
+    #         mock_qDialog.getOpenFileName(filter='PKL files (*.pkl)')[0].return_value = 'Tests/Test.pkl'
+    #         result = self.test_window.load_model()
+    #
+    #         self.assertTrue(result)
+    #         defaultFeatureList = ["WBC", "LYMF", "GRAN", "MID", "RBC", "HGB", "MCH", "MCHC", "MPV", "PLT"]
+    #
+    #         # self.assertEqual("Test", self.test_window.modelName)
+    #         self.assertEqual(defaultFeatureList, self.test_window.featureList)
+    #         mock_load.assert_called_once()
+    #         mock_update.assert_called_once()
+    #
+    #
+    #
+    # @mock.patch("joblib.load")
+    # @mock.patch("GUI.predictionWindow.pop_message_box")
+    # def test_load_model_fail(self, mock_load, mock_pop):
+    #     with patch('PyQt6.QtWidgets.QFileDialog.getOpenFileName') as mock_qDialog:
+    #         mock_qDialog.getOpenFileName(filter='PKL files (*.pkl)')[0].return_value = ""
+    #         result = self.test_window.load_model()
+    #         print(result)
+    #         self.assertFalse(result)
 
     def test_get_import(self):
-        self.fail()
+        pass
 
     def test_get_seal_tag(self):
-        self.fail()
+        pass
 
     def test_get_manual_input_prediction(self):
-        self.fail()
+        pass
+
+    def test_get_manual_input_prediction(self):
+        self.test_window.combo1.addItem("Female")
+        self.test_window.seal_tag_input.setText("Julia")
+
+        self.test_window.featureList=defaultFeatureList
+        self.test_window.wbc_input.setText("1")
+        self.test_window.lymf_input.setText("2")
+        self.test_window.gran_input.setText("3")
+        self.test_window.mid_input.setText("4")
+        self.test_window.rbc_input.setText("5")
+        self.test_window.hgb_input.setText("6")
+        self.test_window.mch_input.setText("7")
+        self.test_window.mchc_input.setText("8")
+        self.test_window.mpv_input.setText("9")
+        self.test_window.plt_input.setText("10")
+
+        self.test_window.featureInputDict = dict(zip(defaultFeatureList,self.test_window.default_input_list))
+        output = ['Julia', 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 0, 0]
+
+        self.test_window.get_manual_input_prediction()
+
+        self.assertEqual(output,self.test_window.sealDataManualInput)
 
 
 if __name__ == '__main__':
