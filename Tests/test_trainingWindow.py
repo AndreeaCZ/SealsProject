ca6d2edb57fc81d4302352755f84bc22767014d0
Julia Dahlberg <j.i.k.dahlberg@student.rug.nl>
2022-06-16 20:05:09 +0200

    TST: Added tests for training window

--- Tests/test_trainingWindow.py
+++ Tests/test_trainingWindow.py
@@ -2,16 +2,18 @@ import sys
 import unittest
 from unittest import TestCase, mock
 
-from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtCore import Qt, QSize, qDebug
 from PyQt6.QtTest import QTest
 from PyQt6.QtWidgets import QApplication
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 from GUI.trainingWindow import TrainModelWindow
 
 ########################################################################################################################
 # File for testing the training window
 ########################################################################################################################
-
+from Model.modelCreation import data_preprocessing
 
 app = QApplication(sys.argv)
 
@@ -28,8 +30,6 @@ class TestTrainingWindow(TestCase):
     def test_title(self):
         self.assertEqual(self.test_window.windowTitle(), "Train a model")
 
-    def test_size(self):
-        self.assertEqual(self.test_window.size(), QSize(500, 450))
 
     def test_train_button_clicked(self):
         with mock.patch('GUI.trainingWindow.TrainModelWindow.train_new_model') as clickCheck:
@@ -53,22 +53,49 @@ class TestTrainingWindow(TestCase):
             test_window = TrainModelWindow(m)
             home_button = test_window.home_button
             QTest.mouseClick(home_button, Qt.MouseButton.LeftButton)
-            self.assertTrue(clickCheck.called)
+            self.assertTrue(clickCheckgit .called)
 
-    # 1. test training a new model with checked parameters.
-    # NOT IMPLEMENTED YET
-    def test_train_new_model(self):
-        self.fail()
+    @mock.patch("joblib.load")
+    @mock.patch("GUI.trainingWindow.pop_message_box")
+    def test_train_new_model(self ,mock_load, mock_pop):
+        # Before training a model, the model should be set to none
+        self.assertIsNone(self.test_window.model)
 
-    # 2. test saving the training model into a local repository.
-    # NOT IMPLEMENTED YET
-    def test_save_features(self):
-        self.fail()
+        # Train new model with wbc
+        self.test_window.wbc.click()
+        self.assertTrue(self.test_window.wbc.isChecked())
+        self.test_window.train_new_model()
+        self.assertIsNotNone(self.test_window.model)
 
-    # 3. test saving the training model without name.
-    # NOT IMPLEMENTED YET
-    def test_empty_model_name(self):
-        self.fail()
+    @mock.patch("joblib.load")
+    @mock.patch("GUI.trainingWindow.pop_message_box")
+    def test_display_confusion_matrix(self,mock_load, mock_pop):
+        # Choosing features
+        self.test_window.wbc.click()
+        self.test_window.lymf.click()
+        self.test_window.gran.click()
+        self.test_window.mid.click()
+        self.test_window.rbc.click()
+
+        # After training the model, confusion matrix should not be empty
+        self.assertIs(self.test_window.confusion_matrix.text(),"")
+        self.test_window.train_new_model()
+        self.assertIsNot(self.test_window.confusion_matrix.text(),"")
+
+    @mock.patch("joblib.load")
+    @mock.patch("GUI.trainingWindow.pop_message_box")
+    def test_display_feature_importance(self,mock_load, mock_pop):
+        # Choosing features
+        self.test_window.mch.click()
+        self.test_window.lymf.click()
+        self.test_window.gran.click()
+        self.test_window.hgb.click()
+        self.test_window.rbc.click()
+
+        # After training the model, feature importance should not be empty
+        self.assertEqual(self.test_window.feature_importance.text(),"Feature importance")
+        self.test_window.train_new_model()
+        self.assertNotEqual(self.test_window.feature_importance.text(),"Feature importance")
 
 
 if __name__ == '__main__':
